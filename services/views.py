a6ae4b9564ccf234b3cb332968071571fa9d40b7
patricksingleton86 <patricksingleton95@gmail.com>
2021-03-24 13:38:44 +0000

    Added category filtering to services

--- services/views.py
+++ services/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Service, Category
 # Create your views here.
 
 def all_services(request):
     """ A view to show all services """
 
+    services = Service.objects.all()
     categories = Category.objects.all()
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            services = services.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
+        'services': services,
         'categories': categories,
+        'current_categories': categories,
     }
 
     return render(request, 'services/services.html', context)
