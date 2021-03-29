8268f317b24dfb9c39046fdf0ae24c74c1e20791
patricksingleton86 <patricksingleton95@gmail.com>
2021-03-29 01:50:36 +0000

    Removed secret key and set debug

--- red_door/settings.py
+++ red_door/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_*2s#g-i22ggj!+(e+eoyu_*5)kr(gg6x2u4lgklu(7(!urbt='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['red-door-beauty-studio.herokuapp.com', 'localhost']
 
